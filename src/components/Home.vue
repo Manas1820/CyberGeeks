7f8765f737f60580dc4fce7bb31ef046975c182d
Manas1820 <manasgupta1820@gmail.com>
2021-03-30 01:41:38 +0530

    ✨ Adding Main screen

---
+++ src/components/Home.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="flex-container">
+    
+    <div class="name-home"> We Are <vue-typer
+      :text='["CyberGeeks","ComputerizedSmartAlacs","CyberSpaceNerds"]'
+      :repeat='Infinity'
+      initial-action='typing'
+      :pre-type-delay='90'
+      :type-delay='110'
+      :pre-erase-delay='2000'
+      :erase-delay='250'
+      erase-style='clear'
+      :erase-on-complete='true'
+      caret-animation='smooth'
+      ></vue-typer></div>
+    <div class="text-desc">
+      <p> </p>
+    </div>
+  </div>    
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.name-home{
+  font-size: 3rem;
+  font-family: Shrimp;
+  text-align: center;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+ background: -webkit-linear-gradient(48deg,#69C9C6,#F3F4F4);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+.text-desc{
+  font-size: 1.2rem;
+  font-family: Archia;
+  text-align: center;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #F5F3F5;
+}
+.flex-container{
+  float: center;
+  color: aliceblue;
+}
+
+.vue-typer {
+  font-family:Shrimp; 
+}
+.vue-typer .custom.char.typed {
+  color: #009688;
+}
+.vue-typer .custom.char.selected {
+  color: #E91E63;
+}
+
+
+</style>
+
+
---
+++ src/components/Main.vue
@@ -0,0 +1,36 @@
+<template>
+    <div> 
+      <Header/>
+      <b-container>
+        <div class="add-space">
+            <Home/> 
+        </div>
+        <div class="add-space">
+            <Projects/>
+        </div>     
+      </b-container>
+   </div>     
+</template>
+
+<script>
+
+import Home from './Home'
+import Header from './Header.vue'
+import Projects from './Hackers.vue'
+
+export default {
+  name: 'Main',
+  components: {
+    Home,
+    Header,
+    Projects
+  }
+}
+
+</script>
+
+<style scoped>
+.add-space {
+  display :flexbox;
+}
+</style>
