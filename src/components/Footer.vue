75206bf13e989610d3c15c8d39d739f3a25eeada
Manas1820 <manasgupta1820@gmail.com>
2021-03-31 00:54:18 +0530

    ⚡ Added Header and Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="Footer">
+    <a
+      href="#" class="link"
+      >© Manas Gupta</a
+    >| Made with 💙 by Manas Gupta
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style scoped>
+
+#Footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  left: 0;
+  bottom: 0.3em;
+  width: 100%;
+  color: white;
+  text-align: center;
+  color:  #f7f7f7;
+  position: fixed;
+}
+
+.link{
+     text-decoration: none;
+    color: #f7f7f7;
+    padding-right: 0.3rem;
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,22 @@
 <template>
-<div>
-    <b-navbar toggleable="lg" type="dark" class="header-style" sticky>
+<div onmousedown="return false" onselectstart="return false">
+    <b-navbar toggleable="lg" type="dark" class="header-style">
       <b-container>
-      <b-navbar-brand href="#" class="logo">CyberGeeks</b-navbar-brand>
-  
+      <b-navbar-brand href="#" class="logo" data-cursor-hover>CyberGeeks</b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav class="all-menu">  
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="https://discord.com/channels/688852585071116289/821081715094061136" class="item-nav"  data-cursor-hover>Discord</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
       </b-container>
     </b-navbar>
 </div>
 </template>
+
+
+//fab fa-discord icon-color
 
 <script>
 export default {
@@ -27,6 +36,10 @@ export default {
   color: #FFFFFF;
 }
 
+.icon-color{
+  color: aqua;
+  size: 5rem;
+}
 .item-nav{
   color: #FFFFFF;
   font-family: Archia;
@@ -39,17 +52,8 @@ export default {
   background: rgba(3, 4, 5, 0.4);
   backdrop-filter: blur(40px);
 }
+
 </style>
 
 
 
-// <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-// <b-collapse id="nav-collapse" is-nav class="all-menu">  
-//         <!-- Right aligned nav items -->
-//         <b-navbar-nav class="ml-auto">
-//           <b-nav-item href="#" class="item-nav">Projects</b-nav-item>
-//           <b-nav-item href="#" class="item-nav">Skills</b-nav-item>
-//           <b-nav-item href="#" class="item-nav">About</b-nav-item>
-//           <b-nav-item href="#" class="item-nav">Contact</b-nav-item>
-//         </b-navbar-nav>
-//       </b-collapse>
